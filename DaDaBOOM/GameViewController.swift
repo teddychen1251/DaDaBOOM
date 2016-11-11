c1c304f7852a70b31dbe5a75ebac9ad2c48135da
Teddy Chen <teddychen@teddys-macbook-air.local>
2016-11-11 17:45:25 -0600

    Implemented AIchanceAdjustor; improved print statements for testing; added AIshots variable

--- DaDaBOOM/GameViewController.swift
+++ DaDaBOOM/GameViewController.swift
@@ -13,6 +13,7 @@ class GameViewController: UIViewController {
     var AIreloadChance = 100
     var AIshootChance = 0
     var AImove = "r"
+    var AIshotsLeft = 0
     
     var yourTurn = false
     var yourMove = "r"
@@ -25,18 +26,22 @@ class GameViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         view.backgroundColor = UIColor.redColor()
+        startRound()
     }
     
     func startRound() {
         view.backgroundColor = UIColor.greenColor()
+        daDaSetUp()
     }
     
     func daDaSetUp() {
         let chance = Int(arc4random_uniform(100))
         if chance < AIreloadChance {
             AImove = "r"
+            AIshotsLeft += 1
         } else if chance < AIshootChance {
             AImove = "s"
+            AIshotsLeft -= 1
         } else {
             AImove = "i"
         }
@@ -46,36 +51,51 @@ class GameViewController: UIViewController {
     func comparator() {
         if AImove == yourMove {
             if yourMove == "s" {
-                print("tie")
+                print("tie" + " AI: \(AImove) You: \(yourMove)")
             } else {
-                print("nothing happens")
+                print("nothing happens" + " AI: \(AImove) You: \(yourMove)")
+                AIchanceAdjustor()
+                daDaSetUp()
             }
         } else if AImove == "i" || yourMove == "i" {
-            print("nothing happens")
+            print("nothing happens" + " AI: \(AImove) You: \(yourMove)")
+            AIchanceAdjustor()
+            daDaSetUp()
         } else if AImove == "r" {
-            print("you win")
+            print("you win" + " AI: \(AImove) You: \(yourMove)")
         } else if yourMove == "r" {
-            print("AI wins")
+            print("AI wins" + " AI: \(AImove) You: \(yourMove)")
+        }
+    }
+    
+    func AIchanceAdjustor() {
+        if AIshotsLeft == 0 {
+            AIreloadChance = 35
+            AIshootChance = 0
+        } else {
+            AIreloadChance = 30 - (3 * AIshotsLeft)
+            AIshootChance = 50 + (2 * AIshotsLeft)
         }
     }
 
     @IBAction func onTappedReloadButton(sender: UIButton) {
         if yourTurn {
+            yourTurn = false
             yourMove = "r"
             shotsLeft += 1
             comparator()
-            yourTurn = false
         }
     }
     @IBAction func onTappedShieldButton(sender: UIButton) {
         if yourTurn {
+            yourTurn = false
             yourMove = "i"
             comparator()
-            yourTurn = false
         }
     }
     @IBAction func onTappedShootButton(sender: UIButton) {
         if yourTurn {
+            yourTurn = false
             if shotsLeft > 0 {
                 yourMove = "s"
                 shotsLeft -= 1
@@ -83,7 +103,6 @@ class GameViewController: UIViewController {
                 yourMove = "r"
             }
             comparator()
-            yourTurn = false
         }
     }
   
